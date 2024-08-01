36c477d183b0ffea6bb17f9e245216586a7e69ae
Wisdom <pdsu.wwz@foxmail.com>
2024-08-01 13:45:37 +0800

    init repo

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devServer: {
+    port: 2234,
+  },
+  devtools: { enabled: true }
+})
